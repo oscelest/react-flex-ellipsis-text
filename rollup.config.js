ba165f7348a554f17f79c185ec52b46a0caa8d97
oscelest <oscelest@gmail.com>
2022-08-06 21:28:36 +0200

    Adding license

--- rollup.config.js
+++ rollup.config.js
@@ -36,7 +36,7 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({tsconfig: "./tsconfig.json"}),
+      typescript({tsconfig: "./tsconfig.build.json"}),
       postcss(),
       terser(),
     ],
